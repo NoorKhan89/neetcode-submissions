0ec141b53747e70209f29b6a6ac5a7a32bbcb6c6
Noor Mohammad Khan <153984679+NoorKhan89@users.noreply.github.com>
2026-05-15 08:57:21 +0530

    Add: linked-list-cycle-detection - submission-3

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-3.java
@@ -0,0 +1,99 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        ListNode curr = head;
+        while(curr != null)
+        {
+            if(set.contains(curr))
+            {
+                return true;
+            }
+            set.add(curr);
+            curr = curr.next;
+        }
+        return false;
+    }
+}
+    
+    /*
+     if(head == null)
+     {
+      return false;
+     }
+     ListNode slow = head;
+     ListNode fast= head.next;
+    
+    while(slow != fast)
+    {
+       if (fast != null || fast.next != null)
+     {
+      return true;
+     }
+
+     slow = slow.next;
+     fast =fast.next.next;
+    }
+    return false;
+    }
+
+}
+     /*
+      Set<ListNode> set = new HashSet<>();
+      ListNode curr =head;
+      while(curr != null)
+      {
+        if(set.contains(curr))
+        {
+            return true;
+        }
+        set.add(curr);
+        curr= curr.next;
+      }
+      return false;
+    }
+}
+      /*
+        Set<ListNode> set= new HashSet<>();
+        ListNode curr = head;
+        while(curr != null)
+        {
+            if(set.contains(curr))
+            {
+                return true;
+            }
+            set.add(curr);
+            curr = curr.next;
+        }
+        return false;
+    }
+}
+*
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)
+            {
+                return true;
+            }
+        }
+        return false;
+
+    }
+}
+*/
+
