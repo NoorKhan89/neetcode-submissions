11ea7ed4890b4c346c4e53bf7229caad29de2b72
Noor Mohammad Khan <153984679+NoorKhan89@users.noreply.github.com>
2026-05-17 23:02:48 +0530

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int mySqrt(int x) {
+      int ans =0;
+
+      for(int nums= 1; nums <= x; nums = nums +1)
+      {
+        long val = (long) nums * nums;
+
+        if(val > x)
+        {
+            break;
+        }
+        ans = nums;
+      }  
+      return ans;
+    }
+}
