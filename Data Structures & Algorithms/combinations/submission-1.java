c34e0c85afba809a037de82f36c2e2c4d17016b8
Noor Mohammad Khan <153984679+NoorKhan89@users.noreply.github.com>
2026-08-05 23:16:12 +0530

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void backtrack(List<Integer>l, int f, int n, int k, List<List<Integer>> res)
+    {
+        if(l.size() == k)
+        {
+            res.add(new ArrayList<>(l));
+            return;
+        }
+        int asl = k - l.size();
+        int options = n-f+1;
+        for(int i=f; i <= f+(options- asl); i++)
+        {
+            l.add(i);
+            backtrack(l, i+1, n, k, res);
+            l.remove(l.size() -1);
+        }
+    }
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> l = new ArrayList<>();
+        backtrack(l, 1, n, k, res);
+        return res;
+    }
+}
